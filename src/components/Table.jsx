cb104635d3c4d5da7291c48465ef740fbf787b11
Davi Freitas <df828316@gmail.com>
2022-04-18 16:05:21 -0300

    corrigido lint + modificado style

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -32,25 +32,23 @@ function Table() {
         </tr>
       </thead>
       <tbody>
-        {data.map((e, index) => {
-          return (
-            <tr key={ index }>
-              <td>{e.name}</td>
-              <td>{e.rotation_period}</td>
-              <td>{e.orbital_period}</td>
-              <td>{e.diameter}</td>
-              <td>{e.climate}</td>
-              <td>{e.gravity}</td>
-              <td>{e.terrain}</td>
-              <td>{e.surface_water}</td>
-              <td>{e.population}</td>
-              <td>{e.films}</td>
-              <td>{e.created}</td>
-              <td>{e.edited}</td>
-              <td>{e.url}</td>
-            </tr>
-          );
-        })}
+        {data.map((e, index) => (
+          <tr key={ index }>
+            <td>{e.name}</td>
+            <td>{e.rotation_period}</td>
+            <td>{e.orbital_period}</td>
+            <td>{e.diameter}</td>
+            <td>{e.climate}</td>
+            <td>{e.gravity}</td>
+            <td>{e.terrain}</td>
+            <td>{e.surface_water}</td>
+            <td>{e.population}</td>
+            <td>{e.films}</td>
+            <td>{e.created}</td>
+            <td>{e.edited}</td>
+            <td>{e.url}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
