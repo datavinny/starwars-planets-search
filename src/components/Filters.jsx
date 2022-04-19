d0d31aae64f56291a85eb2848aa02e20760fd767
Davi Freitas <df828316@gmail.com>
2022-04-19 16:31:35 -0300

    req 5/6 imcompleto

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,9 +1,12 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import TableContext from '../context/tableContext';
 
 function Filters() {
   const [populationFilters, setPopulationFilters] = useState(
     { comparison: 'maior que', column: 'population', value: '0' },
+  );
+  const [columnFilters, setColumnFilters] = useState(
+    ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'],
   );
   const {
     setName, setPopulation, population,
@@ -19,6 +22,34 @@ function Filters() {
       const prevArr = prevState.filterByNumericValues;
       return ({ filterByNumericValues: [...prevArr, populationFilters] });
     });
+  };
+
+  const attColumnOptions = () => {
+    const userFilters = population.filterByNumericValues;
+    let newColumnFilters = [];
+    for (let i = 0; i < userFilters.length; i += 1) {
+      newColumnFilters = columnFilters.filter((e) => e !== userFilters[i].column);
+    }
+    if (newColumnFilters.length > 0) {
+      setColumnFilters(newColumnFilters);
+    }
+  };
+
+  useEffect(() => {
+    attColumnOptions();
+  }, [population]);
+
+  const removeSelectedFilter = (selectedFilter) => {
+    const userFilters = population.filterByNumericValues;
+    const attFilters = userFilters.filter((e) => e !== selectedFilter);
+    setPopulation({ filterByNumericValues: [...attFilters] });
+  };
+
+  const clearFilters = () => {
+    setPopulation({ filterByNumericValues: [] });
+    setColumnFilters(
+      ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water']
+    );
   };
 
   return (
@@ -41,11 +72,7 @@ function Filters() {
         <label htmlFor="column-filter">
           Column-Filter:
           <select onChange={ onChangeValue } data-testid="column-filter" id="column">
-            <option value="population">population</option>
-            <option value="orbital_period">orbital_period</option>
-            <option value="diameter">diameter</option>
-            <option value="rotation_period">rotation_period</option>
-            <option value="surface_water">surface_water</option>
+            {columnFilters.map((e) => <option key={ e } value={ e }>{e}</option>)}
           </select>
         </label>
         <label htmlFor="comparison-filter">
@@ -77,21 +104,28 @@ function Filters() {
         >
           FILTRAR
         </button>
+        <button
+          onClick={ clearFilters }
+          type="button"
+          data-testid="button-remove-filters"
+        >
+          REMOVER FILTROS
+        </button>
       </form>
       <section>
         <p>Applied Filters:</p>
         { population.filterByNumericValues.length
       && population.filterByNumericValues.map((e, i) => (
-        <div key={ i }>
-          <p>
-            { e.column }
-            {' '}
-            { e.comparison }
-            {' '}
-            { e.value }
-          </p>
-          <button type="button">X</button>
-        </div>
+        // <div key={ i }>
+        <p key={ i } data-testid="filter">
+          { e.column }
+          {' '}
+          { e.comparison }
+          {' '}
+          { e.value }
+          <button type="button" onClick={ () => removeSelectedFilter(e) }>X</button>
+        </p>
+        // </div>
       ))}
       </section>
     </div>
